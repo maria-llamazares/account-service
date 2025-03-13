a3564aff1ec0e6ba6cb34611cad69a38bff25898
Maria Llamazares <mariallamazareslopez@gmail.com>
2025-03-13 12:08:04 +0100

    Update code with SonarQube analysis

--- src/test/java/com/banking/account_service/error/GlobalExceptionHandlerTest.java
+++ src/test/java/com/banking/account_service/error/GlobalExceptionHandlerTest.java
@@ -1,32 +1,26 @@
 package com.banking.account_service.error;
 
 import com.banking.account_service.utils.Constants;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
-import java.time.LocalDateTime;
 import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
-public class GlobalExceptionHandlerTest {
+class GlobalExceptionHandlerTest {
 
     @Autowired
     private GlobalExceptionHandler globalExceptionHandler;
@@ -38,15 +32,12 @@ public class GlobalExceptionHandlerTest {
     private MethodArgumentNotValidException mockMethodArgumentException;
 
     @Mock
-    private HttpMessageNotReadableException mockHttpMessage;
-
-    @Mock
     private BindingResult mockBindingResult;
 
     @Mock
     private FieldError mockFieldError;
 
-//    @Test
+    @Test
     void testHandleCustomExceptionTest() {
 
         CustomException customException = new CustomException(
@@ -78,7 +69,7 @@ public class GlobalExceptionHandlerTest {
 
     }
 
-//    @Test
+    @Test
     void testHandleUrlNotFoundTest() {
 
         NoHandlerFoundException ex = new NoHandlerFoundException("GET", "/accounts/iban/balances", new HttpHeaders());
@@ -86,7 +77,7 @@ public class GlobalExceptionHandlerTest {
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
-   // @Test
+   @Test
     void testHandleValidationInputParams() {
 
         when(mockFieldError.getDefaultMessage()).thenReturn("Invalid field");
@@ -96,46 +87,5 @@ public class GlobalExceptionHandlerTest {
         ResponseEntity<CustomErrorResponse> response = globalExceptionHandler.handleValidationInputParams(mockMethodArgumentException, webRequest);
         assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
     }
-
-
-//    @Test
-//    public void testHandleInvalidInput_withNullOrEmptyPath() {
-//        // Mock WebRequest
-//        Mockito.when(webRequest.getDescription(false)).thenReturn("uri=/accounts/iban/balances");
-//
-//        // Simular caso cuando el path es vacío
-//        JsonMappingException jsonMappingExceptionWithEmptyPath = mock(JsonMappingException.class);
-//        Mockito.when(jsonMappingExceptionWithEmptyPath.getPath()).thenReturn(Collections.emptyList()); // Lista vacía
-//        Mockito.when(jsonMappingExceptionWithEmptyPath.getOriginalMessage()).thenReturn("Expected type: BigDecimal");
-//        HttpMessageNotReadableException exceptionWithEmptyPath =
-//                new HttpMessageNotReadableException("Invalid input", jsonMappingExceptionWithEmptyPath);
-//
-//        ResponseEntity<CustomErrorResponse> responseEntityWithEmptyPath =
-//                globalExceptionHandler.handleInvalidInput(exceptionWithEmptyPath, webRequest);
-//
-//        CustomErrorResponse errorResponseWithEmptyPath = responseEntityWithEmptyPath.getBody();
-//
-//        // Validar resultados para el caso de lista vacía
-//        assertEquals(HttpStatus.BAD_REQUEST.value(), responseEntityWithEmptyPath.getStatusCode().value());
-//        assertEquals("Parameter: 'unknown'. Expected type: BigDecimal", errorResponseWithEmptyPath.getMessage());
-//
-//        // Simular caso cuando el path es null
-//        JsonMappingException jsonMappingExceptionWithNullPath = mock(JsonMappingException.class);
-//        Mockito.when(jsonMappingExceptionWithNullPath.getPath()).thenReturn(null); // Path null
-//        Mockito.when(jsonMappingExceptionWithNullPath.getOriginalMessage()).thenReturn("Expected type: BigDecimal");
-//        HttpMessageNotReadableException exceptionWithNullPath =
-//                new HttpMessageNotReadableException("Invalid input", jsonMappingExceptionWithNullPath);
-//
-//        ResponseEntity<CustomErrorResponse> responseEntityWithNullPath =
-//                globalExceptionHandler.handleInvalidInput(exceptionWithNullPath, webRequest);
-//
-//        CustomErrorResponse errorResponseWithNullPath = responseEntityWithNullPath.getBody();
-//
-//        // Validar resultados para el caso de path null
-//        assertEquals(HttpStatus.BAD_REQUEST.value(), responseEntityWithNullPath.getStatusCode().value());
-//        assertEquals("Parameter: 'unknown'. Expected type: BigDecimal", errorResponseWithNullPath.getMessage());
-//
-//
-//    }
 
 }
